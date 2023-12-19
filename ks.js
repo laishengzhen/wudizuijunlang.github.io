04eee33db2df473210cba13015eae3d7c55da3e0
laishengzhen <147300460+laishengzhen@users.noreply.github.com>
2023-12-19 10:47:37 +0800

    Update ks.js

--- ks.js
+++ ks.js
@@ -1,8 +1,8 @@
 var dataSource = {
     author: {
         path: "https://laishengzhen.github.io/img/",
-        picFiles:["ljh.jpg","zzx.jpg","lsz.jpg","zrj.jpg"],
-        name:["李健宏老师","张梓鑫","赖盛桢","张荣军"]
+        picFiles:["ljh.jpg","lsz.jpg","zzx.jpg","zrj.jpg"],
+        name:["李健宏老师","赖盛桢","张梓鑫","张荣军"]
     },
     mp3Files: {
         path: "https://jxzy-dmxy.jxstnu.edu.cn/",
